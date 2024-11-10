13e3984ac67fb79e1e95c786fc86f47d603817b2
ckuzmick <conrad.kuzmick@icloud.com>
2024-11-09 23:50:43 -0500

    Initial Commit

---
+++ apDash/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  apDash
+//
+//  Created by ck on 11/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ apDash/apDashApp.swift
@@ -0,0 +1,17 @@
+//
+//  apDashApp.swift
+//  apDash
+//
+//  Created by ck on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct apDashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
